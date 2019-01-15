29abfc79b8d016011a9e351ed20fd3924ac65d4a
xtx1130 <xtx1130@gmail.com>
2019-01-15 14:50:12 +0800

    add: add main file

---
+++ src/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const cssLint = require('./csslint')
+const jsLint = require('./jslint')
+const vuelint = require('./vuelint')
+
+exports = module.exports = {
+    cssLint: cssLint,
+    jsLint: jsLint,
+    vueLint: vuelint
+}
