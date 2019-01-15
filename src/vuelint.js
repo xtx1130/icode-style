771ac9245e990d571f7197f6a9e13db1e3122bcd
xtx1130 <xtx1130@gmail.com>
2019-01-15 14:50:35 +0800

    del: delete useless codes

--- src/vuelint.js
+++ src/vuelint.js
@@ -1,30 +1,3 @@
-// 'use strict'
-
-// const Linter = require("eslint").Linter
-// const fs = require('fs')
-// const rules = require('./rules.json')
-// const parser = require('vue-eslint-parser')
-
-// const linter = new Linter()
-
-// linter.defineParser('vue-eslint-parser', parser)
-
-// exports = module.exports = function (code, filePath) {
-//   let msg = linter.verifyAndFix(code, {
-//     extends: 'plugin:vue/recommended',
-//     parser: 'vue-eslint-parser',
-//     parserOptions: {
-//       sourceType: 'module',
-//       ecmaVersion: 2018,
-//       parser: 'babel-eslint'
-//     }
-//   })
-//   if (msg.fixed){
-//     filePath && fs.writeFileSync(filePath, msg.output, 'utf-8')
-//   }
-//   return msg
-// }
-
 'use strict'
 
 const prettier = require("prettier")
