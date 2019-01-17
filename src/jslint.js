7b4e9d716d09aea61c8cce4f9a4aa5a7e4c7d335
xtx1130 <xtx1130@gmail.com>
2019-01-17 17:31:30 +0800

    features: add comment for js files

--- src/jslint.js
+++ src/jslint.js
@@ -1,13 +1,35 @@
 'use strict'
 
+const parse = require('comment-parser')
 const Linter = require('eslint').Linter
 const fs = require('fs')
+const path = require('path')
 const rules = require('./rules.json')
 
 const linter = new Linter()
 
 exports = module.exports = function (code, filePath) {
+  let commentFlag = false
   let msg
+  let comment = parse(code)
+
+  if(comment.length && comment[0].tags.length){
+    let ta = comment[0].tags
+    for( let i = 0;i < ta.length; i++) {
+      if(ta[i].tag === 'file'){
+        commentFlag = true
+        break;
+      }
+    }
+  }
+  if(!commentFlag && filePath){
+    let comment = [
+      '/**',
+      ' * @file ' + path.parse(filePath).base,
+      ' */'
+    ]
+    code = comment.join('\r\n') + '\r\n' + code
+  }
   if (filePath) {
     msg = linter.verifyAndFix(code, {
       rules,
